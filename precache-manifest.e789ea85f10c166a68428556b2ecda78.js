1d8fd24728425f4f6b58f76e4ebae1427dd4c685
Aleksi Liukkonen <aleksiliukkonen@Aleksis-MacBook-Pro.local>
2018-10-10 17:26:19 +0300

    Updates

--- precache-manifest.e789ea85f10c166a68428556b2ecda78.js
+++ precache-manifest.e789ea85f10c166a68428556b2ecda78.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-movies/static/js/runtime~main.6e096469.js"
   },
   {
-    "revision": "f1b3c4d198c5f7541947",
-    "url": "/react-movies/static/js/main.f1b3c4d1.chunk.js"
+    "revision": "63a823998da9c20c8cb1",
+    "url": "/react-movies/static/js/main.63a82399.chunk.js"
   },
   {
     "revision": "0c6acbbff71520bc4433",
     "url": "/react-movies/static/js/1.0c6acbbf.chunk.js"
   },
   {
-    "revision": "f1b3c4d198c5f7541947",
-    "url": "/react-movies/static/css/main.b9bcdda5.chunk.css"
+    "revision": "63a823998da9c20c8cb1",
+    "url": "/react-movies/static/css/main.1675df79.chunk.css"
   },
   {
-    "revision": "6e58c0d05f0237fb8fe7f78ec64f1f94",
+    "revision": "3742d9c514f3974fec897aef3315fd51",
     "url": "/react-movies/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/react-movies/precache-manifest.775689d436d28b2463c618d433fddc69.js"
+  "/react-movies/precache-manifest.e789ea85f10c166a68428556b2ecda78.js"
 );
 
 workbox.clientsClaim();
